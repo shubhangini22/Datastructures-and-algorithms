f17b681f9afb89e46d26c37f4638c554808c446d
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-07-30 15:23:22 +0530

    Create count set bits.cpp

---
+++ Bits Manipulation/count set bits.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int n) {
+        int count=0;
+        //everytime we are taking the last bit and checking
+        //count number of set bits
+        while(n>0)
+        {
+            int last_bit=n&1;
+            count+=last_bit;
+            n=n>>1;
+            //n right shift 1 will give the next bit required
+        }
+        return count;
+        // Write Your Code her
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
