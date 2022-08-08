97eaa3980f07deba62f319195d5d801b70123965
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-08-08 13:05:44 +0530

    Create merge max heaps.cpp

---
+++ Heaps/merge max heaps.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+// C++ program to merge two max heaps.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+    
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+        vector<int> res;
+         priority_queue<int> q;
+        //two vectors
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        for(int i=0;i<n;i++)
+        {
+            q.push(a[i]);
+        }
+         for(int i=0;i<m;i++)
+        {
+            q.push(b[i]);
+        }  
+       
+       while(!q.empty())
+       {
+           int t=q.top();
+           q.pop();
+           res.push_back(t);
+       }
+       return res;
+        
+        
+        // your code here
+    }
+};
+
+//{ Driver Code Starts.
+
+bool isMerged(vector<int> &arr1, vector<int> &arr2, vector<int> &merged){
+    if (arr1.size() + arr2.size() != merged.size()){
+        return false;
+    }
+    arr1.insert(arr1.end(),arr2.begin(),arr2.end());
+    sort(arr1.begin(),arr1.end());
+    vector<int> mergedCopy =merged;
+    sort(mergedCopy.begin(),mergedCopy.end());
+    if (arr1 != mergedCopy){
+        return false;
+    }
+    for(int i = 1; i<merged.size(); i++){
+        if(merged[i] > merged[(i-1)/2])
+            return false;
+    }
+    return true;
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, i;
+        cin >> n >> m;
+        vector<int> a(n,0),b(m,0);
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (i = 0; i < m; i++) {
+            cin >> b[i];
+        }
+        vector<int> merged, copyA = a, copyB = b;
+        Solution obj;
+        merged = obj.mergeHeaps(a, b, n, m);
+        bool flag = isMerged(copyA, copyB, merged);
+        if(flag == false) cout<<0<<endl;
+        else cout<<1<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
